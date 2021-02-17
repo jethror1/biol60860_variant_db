e406c94b36ff5fd2871adf8fe478bdf4ae8e110b
jethror1 <rainford1995@gmail.com>
2021-02-17 16:53:12 +0000

    add search and uplaod templates and routing

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def home():
+    """Main home page for navigation"""
     return render_template('home.html')
+
+
+@app.route('/upload')
+def upload():
+    """Page for uploading new data to database"""
+    return render_template('upload.html')
+
+
+@app.route('/search')
+def search():
+    """Page for searching database"""
+    return render_template('search.html')
